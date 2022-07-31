1b1246418ca2790f8220760d2e5438eab85dfdb1
Saif Alhaider <saifharith90@gmail.com>
2022-07-31 17:40:19 +0300

    let's hope it works

--- accounting/api/account.py
+++ accounting/api/account.py
@@ -1,12 +1,16 @@
+import collections
+from decimal import Decimal
+from locale import currency
+from unicodedata import name
 from ninja import Router
 from ninja.security import django_auth
 from django.shortcuts import get_object_or_404
-from accounting.models import Account, AccountTypeChoices
+from accounting.models import Account, AccountTypeChoices, Transaction
 from accounting.schemas import AccountOut, FourOFourOut, GeneralLedgerOut
-from typing import List
+from typing import Counter, List
+import itertools
 from django.db.models import Sum, Avg
 from rest_framework import status
-
 from restauth.authorization import AuthBearer
 
 account_router = Router(tags=['account'])
@@ -15,6 +19,7 @@ account_router = Router(tags=['account'])
 @account_router.get("/get_all", response=List[AccountOut])
 def get_all(request):
     return status.HTTP_200_OK, Account.objects.order_by('full_code')
+    
 
 
 @account_router.get('/get_one/{account_id}/', response={
@@ -44,26 +49,11 @@ def get_account_balance(request, account_id: int):
 
     return 200, {'account': account.name, 'balance': list(balance), 'jes': list(journal_entries)}
 
-
-@account_router.get('/account-balances/', response=List[GeneralLedgerOut])
-def get_account_balances(request):
-    accounts = Account.objects.all()
-    result = []
-    for a in accounts:
-        result.append({
-            'account': a.name, 'balance': list(a.balance())
-        })
-
-    return status.HTTP_200_OK, result
-
-
-
-
 class Balance:
-    def __init__(self, balances):
+    def __init__(self, balances:list[dict]):
         balance1 = balances[0]
         balance2 = balances[1]
-
+        print(balance1)
         if balance1['currency'] == 'USD':
             balanceUSD = balance1['sum']
             balanceIQD = balance2['sum']
@@ -84,4 +74,99 @@ class Balance:
             'currency': 'IQD',
             'sum': self.balanceIQD
         }]
+    
+@account_router.get('/account-balances/', response=List[GeneralLedgerOut])
+def get_account_balances(request):
+    accounts = Account.objects.all()
+    transactions = Transaction.objects.all()
+    result = []
+    d={}
+    counter = 0
+    father =[]
+    father_id =[]
+    
+    dd1={}
+    dd2={}
+    dd3={}
+    dd4 = {}
+    for a in accounts:
+        result.append({
+            'account': a.name,
+            'balance': list(a.balance()),
+            # 'jes':list(a.journal_entries.all())
+        })
+        
+        if a.children.all():
+            father.append(a.name)
+            father_id.append(a.id)
+            sinsOFtheChild = [i.balance() for i in a.children.all()]
+            d["children of parent id => {0}".format([i['parent_id'] for i in a.children.values()][0])] = sinsOFtheChild
+            # d["{0}".format([i['parent_id'] for i in a.children.values()][0])] = sinsOFtheChild
+    balances = {}
+    print(d,end="\n------------------\n")
+    for x in range(len(list(d.keys()))):
+        Balance_USD=0
+        Balance_IQD=0
+        l = d[list(d.keys())[x]]
+        
+        flat_list = [item for sublist in l for item in sublist]
+        
+        for z in flat_list:
+            final_balance =[]
+            if z['currency'] == "USD":
+                Balance_USD += z['sum']
+                dd1 = ({"currency":z['currency'],"sum":Balance_USD})
+            elif z['currency'] == "IQD":
+                Balance_IQD += z['sum']
+                dd2 = ({"currency":z['currency'],"sum":Balance_IQD})
+            
+        final_balance = [dd1,dd2]
+        balances.update({father[x]:final_balance})
+    print(balances)
+    for r in result:
+        for k,v in balances.items():
+            if r['account'] == k:
+                r['balance'] = v         
+
+    # the code below was written by a sicopath not recomended to read it or even use it
+    # child ={}
+    # father=[]
+    # data = collections.defaultdict(list)
+    # dd={}
+    # balances = []
+    # for i in accounts.values():
+    #     if i['parent_id'] == None:father.append(i['name'])
+    #     else:child.update({i['name']:i['parent_id']})
+    # combo = {'|'.join(sorted(k for k in child.keys() if child[k] == v)): v for v in set(child.values())}
+    # for z in range(len(result)):
+    #     for j in range(len(list(combo.items()))):
+    #         if result[z]['account'] in list(combo.items())[j][0].split('|'):
+    #             f = [f"{i['currency']}|{i['sum']}" for i in result[z]['balance']]
+    #             data[repr(list(combo.items())[j][0].split('|'))].append(f)
+    # numOfFathers =0
+    # for k,o in data.items():
+    #     l = [i.split("|") for i in list(itertools.chain(*data[k]))]
+    #     currency = [z[0] if z[0] =="USD" else "IQD" for z in l]
+    #     money = [z[1] for z in l]
+    #     # dd.update({"USD Amount":0,"IQD Amount":0})
+    #     usdMoney,iqdMoney =0,0
+    #     for z in range(len(money)):
+    #         if currency[z] == "USD":
+    #             usdMoney += int(money[z])
+    #             dd = ({"children":k,"USD Amount":usdMoney,"IQD Amount":iqdMoney})
+    #         elif currency[z] == "IQD":
+    #             iqdMoney += int(money[z])
+    #             dd = ({"children":k,"USD Amount":usdMoney,"IQD Amount":iqdMoney})
+    #     dd.update({"father":father[numOfFathers]})
+    #     balances.append(dd) 
+    #     numOfFathers = numOfFathers+ 1   
+
+    
+    # for c in range(len(result)):
+    #     for h in balances:
+    #         if result[c]['account'] == h['father']:
+    #             result[c]['balance'].extend([{"currency":"USD","sum":h["USD Amount"]},
+    #             {"currency":"IQD","sum":h["IQD Amount"]}])
+    return status.HTTP_200_OK, result
+
 
