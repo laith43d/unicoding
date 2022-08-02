5f787080d32b6b810d208002a8151f7438194896
ahmedlord20 <103456191+ahmedlord20@users.noreply.github.com>
2022-08-02 14:13:12 +0300

    Update models.py

--- accounting/models.py
+++ accounting/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models import Sum
-from django.dispatch import receiver
-from django.db.models.signals import post_save
+from mptt.models import MPTTModel, TreeForeignKey
 from accounting.exceptions import AccountingEquationError
 
 '''
@@ -48,19 +47,26 @@ class CurrencyChoices(models.TextChoices):
     IQD = 'IQD', 'IQD'
 
 
-class Account(models.Model):
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+class Account(MPTTModel):
+    parent = TreeForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
     type = models.CharField(max_length=255, choices=AccountTypeChoices.choices)
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=20, null=True, blank=True)
     full_code = models.CharField(max_length=25, null=True, blank=True)
     extra = models.JSONField(default=dict, null=True, blank=True)
 
+    class MPTTMeta:
+        order_insertion_by = ['full_code']
+
     def __str__(self):
         return f'{self.full_code} - {self.name}'
 
     def balance(self):
-        return self.journal_entries.values('currency').annotate(sum=Sum('amount')).order_by()
+        result = [
+            account.ournal_entries.values('currency').annotate(sum=Sum('amount')).order_by()
+            for account in self.get_descendants(include_self=True)
+        ]
+        return result
 
     # def save(
     #         self, force_insert=False, force_update=False, using=None, update_fields=None
@@ -89,7 +95,7 @@ class Account(models.Model):
 #         instance.full_code = f'{instance.id}'
 
 
-class Transaction(models.Model):
+class Transaction(MPTTModel):
     type = models.CharField(max_length=255, choices=TransactionTypeChoices.choices)
     description = models.CharField(max_length=255)
 
@@ -100,11 +106,14 @@ class Transaction(models.Model):
             raise AccountingEquationError
 
 
-class JournalEntry(models.Model):
+class JournalEntry(MPTTModel):
     class Meta:
         verbose_name_plural = 'Journal Entries'
 
-    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='journal_entries')
+    class MPTTMeta:
+        order_insertion_by = ['full_code']
+
+    account = TreeForeignKey(Account, on_delete=models.CASCADE, related_name='journal_entries')
     transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, related_name='journal_entries')
     amount = models.DecimalField(max_digits=19, decimal_places=2)
     currency = models.CharField(max_length=3, choices=CurrencyChoices.choices)
