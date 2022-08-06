2e8b8b92b0b1c5968d36e0783e9074dbfef3646e
devsadeq <dev.sadeq@gmail.com>
2022-08-06 14:08:13 +0300

    finish task 4 ✅

--- accounting/utils.py
+++ accounting/utils.py
@@ -1,15 +1,15 @@
 class Balance:
     def __init__(self, balances):
-        balanceIQD = 0
-        balanceUSD = 0
-        for i in balances:
-            if i['currency'] == 'USD':
-                balanceUSD = i['sum']
-            if i['currency'] == 'IQD':
-                balanceIQD = i['sum']
+        balance_iqd = 0
+        balance_usd = 0
+        for balance in balances:
+            if balance['currency'] == 'USD':
+                balance_usd = balance['sum']
+            if balance['currency'] == 'IQD':
+                balance_iqd = balance['sum']
 
-        self.balanceUSD = balanceUSD
-        self.balanceIQD = balanceIQD
+        self.balanceUSD = balance_usd
+        self.balanceIQD = balance_iqd
 
     def __add__(self, other):
         self.balanceIQD += other.balanceIQD
@@ -27,6 +27,15 @@ class Balance:
             return self
         else:
             return self.__add__(other)
+
+    def __gt__(self, other):
+        return self.balanceUSD > other.balanceUSD, self.balanceIQD > other.balanceIQD
+
+    def __lt__(self, other):
+        return self.balanceUSD < other.balanceUSD, self.balanceIQD < other.balanceIQD
+
+    def is_zero(self):
+        return self.balanceUSD == 0 and self.balanceIQD == 0
 
 
 def zero_balances():
