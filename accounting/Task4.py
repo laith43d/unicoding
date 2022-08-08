ec20543578fb41093674091e28920c555c80b44d
Fatima Salah <alanyfsalah@gmail.com>
2022-08-09 00:34:14 +0300

    Last changes in Task4 );

--- accounting/Task4.py
+++ accounting/Task4.py
@@ -24,6 +24,7 @@ class Balance:
             'currency': 'IQD',
             'sum': self.balanceIQD
         }]
+
     def __gt__(self, other):
         
         if self.balanceUSD > other.balanceUSD :
@@ -54,28 +55,57 @@ class Balance:
             return True
         else:
             return False
-    #Here Is The Test <:
-list1= [{
-            'currency': 'USD',
-            'sum': 0
-        }, {
-            'currency': 'IQD',
-            'sum': 0
-        }]
-list2=[{'currency': 'USD',
-            'sum': 400
-        }, {
-            'currency': 'IQD',
-            'sum': 2000020
-        }]
+
+    def is_zero(self):
+        if self.balanceIQD == 0 and self.balanceUSD == 0 :
+            return True
+        else:
+            return False    
+    #Here Is The Test <: 
+
+list1=[{
+        'currency': 'USD',
+        'sum': 500
+    }, {
+        'currency': 'IQD',
+        'sum': 0
+    }]
+list2=[{
+    'currency': 'USD',
+    'sum': 400
+        },{
+        'currency': 'IQD',
+        'sum': 2000020
+    }]
 Obj1 = Balance(list1)
 Obj2 = Balance(list2)
 
-result = (Obj1 > Obj2)
-print(result)
-result2 = (Obj1 < Obj2)
-print(result2)
-result3 = (Obj1 == 0)
-print(result3)
-result4 = (Obj2 == 0)
-print(result4)
+print('Obj1 > Obj2 =>',Obj1 > Obj2) #OUTPUT:Obj1 > Obj2 => ({'USD': True}, {'IQD': False})
+print('Obj2 > Obj1 =>',Obj2 > Obj1) #OUTPUT:Obj2 > Obj1 => ({'USD': False}, {'IQD': True})
+
+print('Obj1 < Obj2 =>',Obj1 < Obj2) #OUTPUT:Obj1 < Obj2 => ({'USD': False}, {'IQD': True})
+print('Obj2 < Obj1 =>',Obj2 < Obj1) #OUTPUT:Obj2 < Obj1 => ({'USD': True}, {'IQD': False})
+
+print('Obj1 ==0 =>',Obj1.is_zero()) #OUTPUT:Obj1 ==0 => False
+print('Obj2 ==0 =>',Obj2.is_zero()) #OUTPUT:Obj2 ==0 => False
+            
+''''''''''
+    #Another Sol For Task4,From Internet 
+    def __gt__(self, other):
+        if (isinstance(other,Balance)):
+            StaUSD = self.balanceUSD > other.balanceIQD
+            StaIQD = self.balanceIQD > other.balanceUSD
+            return {'USD':StaUSD},{'IQD':StaIQD}
+     
+    
+        
+    def __lt__(self, other):
+        if (isinstance(other,Balance)):
+            StaUSD = self.balanceUSD < other.balanceIQD
+            StaIQD = self.balanceIQD < other.balanceUSD
+            return {'USD':StaUSD},{'IQD':StaIQD}
+    
+    def is_zero(self):
+        return self.balanceIQD == 0 and self.balanceUSD == 0 
+    
+   '''''''''
